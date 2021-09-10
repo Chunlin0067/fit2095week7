897963837a370627b1e176e769e8ac87afde2445
Chunlin0067 <90006180+Chunlin0067@users.noreply.github.com>
2021-09-10 08:07:45 +0800

    Add files via upload

---
+++ routers/actor.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+const Actor = require('../models/actor');
+const Movie = require('../models/movie');
+module.exports = {
+    getAll: function (req, res) {
+        Actor.find(function (err, actors) {
+            if (err) {
+                return res.status(404).json(err);
+            } else {
+                res.json(actors);
+            }
+        });
+    },
+    createOne: function (req, res) {
+        let newActorDetails = req.body;
+        newActorDetails._id = new mongoose.Types.ObjectId();
+        let actor = new Actor(newActorDetails);
+        actor.save(function (err) {
+            res.json(actor);
+        });
+    },
+    getOne: function (req, res) {
+        Actor.findOne({ _id: req.params.id })
+            .populate('movies')
+            .exec(function (err, actor) {
+                if (err) return res.status(400).json(err);
+                if (!actor) return res.status(404).json();
+                res.json(actor);
+            });
+    },
+    updateOne: function (req, res) {
+        Actor.findOneAndUpdate({ _id: req.params.id }, req.body, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+            res.json(actor);
+        });
+    },
+    deleteOne: function (req, res) {
+        Actor.findOneAndRemove({ _id: req.params.id }, function (err) {
+            if (err) return res.status(400).json(err);
+            res.json();
+        });
+    },
+    addMovie: function (req, res) {
+        Actor.findOne({ _id: req.params.id }, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+            Movie.findOne({ _id: req.body.id }, function (err, movie) {
+                if (err) return res.status(400).json(err);
+                if (!movie) return res.status(404).json();
+                actor.movies.push(movie._id);
+                actor.save(function (err) {
+                    if (err) return res.status(500).json(err);
+                    res.json(actor);
+                });
+            })
+        });
+    },
+    //lab
+    //Delete an actor by its ID and all its movies from the 'Movie' collection.
+    delActnMov: function (req, res) {
+        Actor.findOneAndRemove({ _id: req.params.id }, function (err) {
+            if (err) return res.status(400).json(err);
+            Movie.deleteMany({actors:{$in: req.params.id }}, (err, res) => {
+                if (err) return res.status(400).json(err);
+             })
+
+            res.json();
+        });
+    },
+    //Remove a movie from the list of movies of an actor
+    removeMov:function(req,res){
+        Actor.findOne({_id:req.params.actid},function(err,actor){
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+            for(let i=0; i<actor.movies.length;i++){
+                if(actor.movies[i]==req.params.movid){
+                    actor.movies.splice(i,1);
+                    actor.save(function(err){
+                        return res.json(actor);
+                    })
+                }
+            }
+            res.status(404).json({errMsg:"Movie not found"});
+        })
+    }
+    
+};
